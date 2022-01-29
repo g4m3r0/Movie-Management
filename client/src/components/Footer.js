d5aa43503687bb29428d9cb1b681e239b0dcc231
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-01-29 15:42:00 +0100

    Create Footer.js

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+
+import  { NavLink } from "react-router-dom";
+
+function Footer() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-5">
+            <span class="navbar-text">AMD-Project WS21/22 - Robert Schulz, Lucas Schmutzler</span>
+        </nav>
+    )
+}
+
+export default Footer;
