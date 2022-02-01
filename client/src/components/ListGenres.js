8c47ee8487642d7cd0b3178705eb6d89eda103c6
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-02-02 00:51:03 +0100

    Create ListGenres.js

---
+++ client/src/components/ListGenres.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useState, useEffect } from "react";
+
+const ListGenre = () => {
+
+    const [genres, setGenres] = useState([]);
+
+    async function getGenres() {
+        const res = await fetch("http://localhost:5000/genre");
+
+        const genreArray = await res.json();
+        setGenres(genreArray);
+        console.log(genreArray);
+    }
+
+    async function deleteGenre(id){
+        try {
+            // send request to the backend to delete the record
+            const res = await fetch(`http://localhost:5000/genre/${id}`, {
+                method: "DELETE"
+            });
+
+            // remove item from the table
+            setGenres(genres.filter(genre => genre.id != id));
+            console.log(res);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    // Runs any time the component is rendered
+    useEffect(() => {
+        getGenres();
+    }, []);
+
+    return (
+        <Fragment>
+            <h1 className="text-center my-5">List Genres</h1>
+            <table className="table mt-5">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Genre Name</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    genres.map(genre => (
+                        <tr key={genre.id}>
+                            <td>{genre.id}</td>
+                            <td>{genre.genre_name}</td>
+                            <td><button className="btn btn-danger" onClick={() => deleteGenre(genre.id)}>Delete</button></td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+            </table>
+        </Fragment>
+    );
+}
+
+export default ListGenre;
