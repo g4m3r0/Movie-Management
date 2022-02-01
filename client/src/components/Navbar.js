7e6381a2ece7b0e975ce073ece4202a6fdf640b9
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-02-01 01:04:18 +0100

    Add rate movie

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,11 +5,12 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <NavLink className="navbar-brand" exact to="/">Movie Management</NavLink>
-            <div class="navbar-nav ml-auto">
+            <div className="navbar-nav ml-auto">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item"><NavLink className="nav-link" exact to="/">Home</NavLink></li>
                     <li className="nav-item"><NavLink className="nav-link" to="/movie">Movie</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" to="/suggest">Suggestions</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="/rating">Rating</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="/suggest">Suggestion</NavLink></li>
                     <li className="nav-item"><NavLink className="nav-link" to="/person">Person</NavLink></li>
                 </ul>
             </div>
