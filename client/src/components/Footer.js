5fa58d76f9aeca1101ca336236bb63ecc22bec8e
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-02-01 01:04:21 +0100

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,8 +3,8 @@ import  { NavLink } from "react-router-dom";
 
 function Footer() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-5">
-            <span class="navbar-text">AMD-Project WS21/22 - Robert Schulz, Lucas Schmutzler</span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mt-5">
+            <span className="navbar-text">AMD-Project WS21/22 - Robert Schulz, Lucas Schmutzler</span>
         </nav>
     )
 }
