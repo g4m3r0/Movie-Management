0abaeb6a30292591faa853820287976f8d115811
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-02-03 17:19:43 +0100

    Fix navbar exact error

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,17 +4,17 @@ import  { NavLink } from "react-router-dom";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <NavLink className="navbar-brand" exact to="/">Movie Management</NavLink>
+            <NavLink className="navbar-brand" exact="true" to="/">Movie Management</NavLink>
             <div className="navbar-nav ml-auto">
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item"><NavLink className="nav-link" exact to="/">Home</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" to="/movie">Movie</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" to="/genre">Genre</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" to="/genrerelation">Genre Relation</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" to="/rating">Rating</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" to="/suggest">Suggestion</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" to="/person">Person</NavLink></li>
-                    <li className="nav-item"><NavLink className="nav-link" to="/role">Role</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" exact="true" to="/">Home</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" exact="true" to="/movie">Movie</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" exact="true" to="/genre">Genre</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" exact="true" to="/genrerelation">Genre Relation</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" exact="true" to="/rating">Rating</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" exact="true" to="/suggest">Suggestion</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" exact="true" to="/person">Person</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" exact="true" to="/role">Role</NavLink></li>
                 </ul>
             </div>
         </nav>
