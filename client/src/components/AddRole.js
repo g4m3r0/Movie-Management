9332bb684e415ebeca081dde14e6f0d2d39485bc
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-02-03 23:18:05 +0100

    Add role dropdown selector

--- client/src/components/AddRole.js
+++ client/src/components/AddRole.js
@@ -1,9 +1,12 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { handleError } from './Helpers'
 
 const InputRole = () => {
 
     const [inputs, setInputs] = useState({});
+    const [movies, setMovies] = useState([]);
+    const [persons, setPersons] = useState([]);
+    const [roles, setRoles] = useState([]);
 
     const handleChange = (event) => {
         const name = event.target.name;
@@ -26,29 +29,73 @@ const InputRole = () => {
         }
       }
 
+      async function getPersons() {
+        const res = await fetch("http://localhost:5000/person");
+
+        const personArray = await res.json();
+        setPersons(personArray);
+        console.log(personArray);
+    }
+
+    async function getMovies() {
+        const res = await fetch("http://localhost:5000/movie");
+
+        const movieArray = await res.json();
+        setMovies(movieArray);
+        console.log(movieArray);
+    }
+
+    async function getRoles() {
+        const res = await fetch("http://localhost:5000/role");
+
+        const rolesArray = await res.json();
+        setRoles(rolesArray);
+        console.log(rolesArray);
+    }
+
+    // Runs any time the component is rendered
+    useEffect(() => {
+        getPersons();
+        getMovies();
+        getRoles();
+    }, []);
+
     return (
         <Fragment>
             <h1 className="text-center my-5">Add Role</h1>
             <form onSubmit={handleSubmit}>
                 <div className="form-group my-3">
-                    <label>Person ID</label>
-                        <input 
-                            className="form-control"
-                            type="number" 
-                            name="personId"
-                            value={inputs.personId || ""} 
-                            onChange={handleChange}
-                        />
+                    <label>Person</label>
+                    <select name="personId" onChange={handleChange} className="form-select form-select-sm">
+                        <option selected disabled>
+                            Choose a Person
+                        </option>
+                        {persons.map(person => 
+                            <option value={person.id}>{person.first_name + " " + person.last_name}</option>
+                            )}
+                    </select>
                 </div>
                 <div className="form-group my-3">
-                    <label>Movie ID</label>
-                        <input 
-                            className="form-control"
-                            type="number" 
-                            name="movieId"
-                            value={inputs.movieId || ""} 
-                            onChange={handleChange}
-                        />
+                    <label>Movie</label>
+                    <select name="movieId" onChange={handleChange} className="form-select form-select-sm">
+                        <option selected disabled>
+                            Choose a Person
+                        </option>
+                        {movies.map(movie => 
+                            <option value={movie.id}>{movie.title + " (" + movie.release_year + ")"}</option>
+                            )}
+                    </select>
+                </div>
+                <div className="form-group my-3">
+                    <label>Role</label>
+                    <select name="roletodo" onChange={handleChange} className="form-select form-select-sm">
+                        <option selected disabled>
+                            Choose a Role
+                        </option>
+                        {roles.map(role => 
+                            <option value={role.role_type}>{role.role_type}</option>
+                            )}
+                    </select>
                 </div>
                 <div className="form-group my-3">
                     <label>Role Type</label>
