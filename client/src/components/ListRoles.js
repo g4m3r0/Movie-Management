869322f63c9382fd1c47c9095a0d79b5c422b2ab
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-02-06 00:59:40 +0100

    Update ListRoles.js

--- client/src/components/ListRoles.js
+++ client/src/components/ListRoles.js
@@ -44,9 +44,9 @@ const ListRoles = () => {
             <thead>
                 <tr>
                     <th scope="col">Id</th>
-                    <th scope="col">Person ID</th>
-                    <th scope="col">Movie ID</th>
-                    <th scope="col">Role Type</th>
+                    <th scope="col">Person</th>
+                    <th scope="col">Movie Title</th>
+                    <th scope="col">Role</th>
                     <th scope="col">Delete</th>
                 </tr>
             </thead>
@@ -55,8 +55,8 @@ const ListRoles = () => {
                     roles.map(roles => (
                         <tr key={roles.id}>
                             <td>{roles.id}</td>
-                            <td>{roles.person_id}</td>
-                            <td>{roles.movie_id}</td>
+                            <td>{roles.person}</td>
+                            <td>{roles.movie_title}</td>
                             <td>{roles.role_type}</td>
                             <td><button className="btn btn-danger" onClick={() => deleteRoles(roles.id)}>Delete</button></td>
 
