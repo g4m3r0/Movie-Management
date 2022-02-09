81cf04a65ed9ef13457ce42d1635db2ba82b3129
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-02-09 21:45:01 +0100

    refactor rating

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 var person = require('./routes/person');
 var movie = require('./routes/movie');
+var rating = require('./routes/rating');
 
 // Film Related Person
 app.post('/person', person.add);
@@ -25,78 +26,18 @@ app.delete('/person/:id', person.delete);
 
 
 // Movie
-app.post('/movie', person.add);
-app.get('/movie', person.get);
-app.get('/movie/:id', person.getSingle);
-app.put('/movie/:id', person.update);
-app.delete('/movie/:id', person.delete);
+app.post('/movie', movie.add);
+app.get('/movie', movie.get);
+app.get('/movie/:id', movie.getSingle);
+app.put('/movie/:id', movie.update);
+app.delete('/movie/:id', movie.delete);
 
-// Add rating
-app.post("/rating", async(req, res) => {
-    try {
-        const { username, movieId, rating } = req.body;
-        const newRating = await pool.query(
-            "Call create_or_update_rating(null, $1, $2, $3)", [movieId, rating, username]);
-
-        res.json("Rating was added!");
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
-
-// Get rating
-app.get("/rating", async(req, res) => {
-    try {
-        const allRatings = await pool.query("SELECT * FROM rating_view");
-
-        res.json(allRatings.rows);
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
-
-app.get("/rating/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const rating = await pool.query("SELECT * FROM rating_view WHERE id = $1", [id]);
-
-        res.json(rating.rows);
-
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
-
-// Update rating
-app.put("/rating/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const { username, movieId, rating } = req.body;
-        const updateRating = await pool.query(
-            "Call create_or_update_rating($1, $2, $3, $4)", [id, movieId, rating, username]);
-
-        res.json("Rating was updated!");
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
-
-// Delete rating
-app.delete("/rating/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const deleteRating = await pool.query("DELETE FROM mm_rating WHERE id = $1", [id]);
-
-        res.json("Rating was deleted!");
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
+// Rating
+app.post('/rating', rating.add);
+app.get('/rating', rating.get);
+app.get('/rating/:id', rating.getSingle);
+app.put('/rating/:id', rating.update);
+app.delete('/rating/:id', rating.delete);
 
 // Get suggestion
 app.get("/suggest/:username", async(req, res) => {
