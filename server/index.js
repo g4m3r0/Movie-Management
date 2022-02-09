70dec757b18439b4717c0ee044957f867d00287f
g4m3r0 <9322695+g4m3r0@users.noreply.github.com>
2022-02-09 22:05:18 +0100

    refactor role

--- server/index.js
+++ server/index.js
@@ -58,57 +58,10 @@ app.get('/genrerelation', genrerelation.get);
 app.delete('/genrerelation/:id', genrerelation.delete);
 
 // Role
-
-// Add role
-app.post("/role", async(req, res) => {
-    try {
-        const { personId, movieId, role_type } = req.body;
-        const newRole = await pool.query(
-            "Call create_or_update_role(null, $1, $2, $3)", [personId, movieId, role_type]);
-
-        res.json("Role was added!");
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
-
-// Get roles
-app.get("/role", async(req, res) => {
-    try {
-        const allGenres = await pool.query("SELECT * FROM role_view");
-
-        res.json(allGenres.rows);
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
-
-// Get roles distinct
-app.get("/role/distinct", async(req, res) => {
-    try {
-        const allDistinctRoles = await pool.query("SELECT DISTINCT role_type FROM role_view");
-
-        res.json(allDistinctRoles.rows);
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
-
-// Delete role
-app.delete("/role/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const deleteGenre = await pool.query("DELETE FROM mm_role WHERE id = $1", [id]);
-
-        res.json("Role was deleted!");
-    } catch (error) {
-        res.json("Error:" + error.message);
-        console.error(error);
-    }
-});
+app.post('/role', role.add);
+app.get('/role', role.get);
+app.get('/role/distinct', role.getDistinct);
+app.delete('/role/:id', role.delete);
 
 // Get users
 app.get("/user", async(req, res) => {
